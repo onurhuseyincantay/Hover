823ec4c6455cdc280ef28744f3c53fa39a9ca8d4
Onur Hüseyin Çantay <cantay@d-tt.nl>
2019-07-08 16:24:21 +0200

    Initial Commit

---
+++ AccNetworkProvider/AccNetworkProvider.h
@@ -0,0 +1,19 @@
+//
+//  AccNetworkProvider.h
+//  AccNetworkProvider
+//
+//  Created by Onur Hüseyin Çantay on 8.07.2019.
+//  Copyright © 2019 Onur Hüseyin Çantay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccNetworkProvider.
+FOUNDATION_EXPORT double AccNetworkProviderVersionNumber;
+
+//! Project version string for AccNetworkProvider.
+FOUNDATION_EXPORT const unsigned char AccNetworkProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccNetworkProvider/PublicHeader.h>
+
+
