1c6aad8ae1c7e85ad099a25dc0bd9d624f2dab28
Onur <ohc3807@gmail.com>
2019-12-23 11:28:48 +0100

    minor changes

--- Hover/URLRequest+prepareRequest.swift
+++ Hover/URLRequest+prepareRequest.swift
@@ -9,27 +9,29 @@
 import Foundation
 
 internal extension URLRequest {
-    
-    mutating func prepareRequest(with target: NetworkTarget) {
-            let contentTypeHeaderName = "Content-Type"
-            allHTTPHeaderFields = target.headers
-            allowsCellularAccess = false
-            setValue(target.contentType?.rawValue, forHTTPHeaderField: contentTypeHeaderName)
-            prepareAuthorization(with: target.providerType)
-            httpMethod = target.methodType.methodName
+  
+  private var headerField: String { "Authorization" }
+  private var contentTypeHeader: String { "Content-Type" }
+  
+  mutating func prepareRequest(with target: NetworkTarget) {
+    let contentTypeHeaderName = contentTypeHeader
+    allHTTPHeaderFields = target.headers
+    allowsCellularAccess = false
+    setValue(target.contentType?.rawValue, forHTTPHeaderField: contentTypeHeaderName)
+    prepareAuthorization(with: target.providerType)
+    httpMethod = target.methodType.methodName
+  }
+  
+  private mutating func prepareAuthorization(with authType: AuthProviderType) {
+    switch authType {
+    case .basic(let username, let password):
+      let loginString = String(format: "%@:%@", username, password)
+      guard let data = loginString.data(using: .utf8) else { return }
+      setValue("Basic \(data.base64EncodedString())", forHTTPHeaderField: headerField)
+    case .bearer(let token):
+      setValue("Bearer \(token)", forHTTPHeaderField: headerField)
+    case .none: break
     }
-    
-    private mutating func prepareAuthorization(with authType: AuthProviderType) {
-        let headerField = "Authorization"
-        switch authType {
-        case .basic(let username, let password):
-            let loginString = String(format: "%@:%@", username, password)
-            guard let data = loginString.data(using: .utf8) else { return }
-            setValue("Basic \(data.base64EncodedString())", forHTTPHeaderField: headerField)
-        case .bearer(let token):
-            setValue("Bearer \(token)", forHTTPHeaderField: headerField)
-        case .none: break
-        }
-    }
-    
+  }
+  
 }
