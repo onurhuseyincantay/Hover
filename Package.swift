c76586e18e9032e2574ad62c27d024553ee63dc2
Onur <ohc3807@gmail.com>
2020-01-10 15:08:27 +0100

    minor change

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
             targets: ["Hover"]),
     ],
     dependencies: [
-       .package(url: "https://github.com/onurhuseyincantay/Hover", from: "1.0.2")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
